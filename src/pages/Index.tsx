6668a9eefe82e43548ef33690c30d5f146c86a99
bushwacka00 <bushwacka000@hotmail.com>
2026-03-30 11:35:17 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -110,7 +110,7 @@ const testimonials = [
   },
   {
     subject: "Rustieke oude eiken vloer uit oud pand hergebruikt.",
-    text: "Wij wilden in ons huis alle vloerbedekking vervangen voor een eiken vloer. Ons budget was echter beperkt en zochten naar een alternatief. \nWoodykozijnen kwam met een hele intressante oplossing: een oude gebruikte eiken vloer opkopen. Woody zocht naar geschikte gebruikte vloerplanken. In eerste instantie zag de oude eiken vloer er niet aantrekkelijk uit. De vloer was verkleurd en oud. Na het leggen van de eiken planken werd de vloer opgeschuurd met een vloerschuurmachine en daarna opnieuw geolied. \nWij hadden nooit verwacht dat een oude eiken vloer als een nieuwe, prachtige warme vloer zou uitpakken en wij waren enorm verwonderd. De vloer werd rondom afgewerkt met parketveren en mooie plinten. Vakmanschap! En dat alles voor 1/4 van de prijs van een nieuwe eiken vloer. Zo blij mee!",
+    text: "Wij wilden in ons huis alle vloerbedekking vervangen voor een eiken vloer. Ons budget was echter beperkt en zochten naar een alternatief. \nWoodykozijnen kwam met een hele intressante oplossing: een oude gebruikte eiken vloer opkopen. Woody zocht naar gebruikte vloerplanken die geschikt waren. In eerste instantie zag de oude eiken vloer er niet aantrekkelijk uit. De vloer was verkleurd en oud. Na het leggen van de eiken planken werd de vloer opgeschuurd met een vloerschuurmachine en daarna opnieuw geolied. \nWij hadden nooit verwacht dat een oude eiken vloer als een nieuwe, prachtige warme vloer zou uitpakken en wij waren enorm verwonderd. De vloer werd rondom afgewerkt met parketveren en mooie plinten. Vakmanschap! En dat alles voor 1/4 van de prijs van een nieuwe eiken vloer. Zo blij mee!",
     author: "Hanna en Frans"
   }
 ];
